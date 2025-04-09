b115e2d3f9c3b67853f273c7e93570ee97f58586
Hassan Ezechiel Diarrassouba <116614661+mickael234@users.noreply.github.com>
2025-04-09 16:25:54 -0400

    new modif

--- app/styles/GlobalStyles.ts
+++ app/styles/GlobalStyles.ts
@@ -16,7 +16,7 @@ const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
-    font-family: &apos;Inter&apos;, sans-serif;
+    font-family: 'Inter', sans-serif;
     background-color: var(--background);
     color: var(--text);
     line-height: 1.6;
